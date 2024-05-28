1a8165fb3c0f66212ce337ff11f2e6c885c07e82
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-05-28 12:30:44 +0200

    implementing ES6's for ...of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-	const res = [];
-	for (const element of array){
-		let value = appendString + element;
-		res.push(value);
+  const res = [];
+  for (const word of array) {
+    res.push(appendString + word);
   }
+
   return res;
 }
