cfe30a66af5fa0835a1461b4da1af80e1ad04f48
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-05-30 08:38:21 +0200

    task2

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,14 +1,17 @@
 export default function createReportObject(employeesList) {
     return {
-        allEmployees: employeesList,
-
-            getNumberOfDepartments(allEmployees) {
-                let i = 0;
-                
-                for (const dep in allEmployees) {
-                    i = i+1;
-                }
-                return i;
-            },
-        };
-}
+  
+      allEmployees: employeesList,
+  
+      getNumberOfDepartments(allEmployees) {
+        let count = 0;
+  
+        // eslint-disable-next-line guard-for-in, no-unused-vars
+        for (const dep in allEmployees) {
+          i += 1;
+        }
+  
+        return i;
+      },
+    };
+  }
