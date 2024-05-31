c61f7ef62ab0b34797ea4f3d105839a4bf7634fa
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-05-31 12:21:36 +0200

    Adding a readme file

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,9 @@
 export default function getResponseFromAPI() {
+    // eslint-disable-next-line no-unused-vars
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('API response');
-        }, 1000);
+      setTimeout(() => {
+        const uploadPhoto = { status: 200, body: 'photo-profile-1' };
+        resolve(uploadPhoto);
+      }, 2000);
     });
-}
+  }
