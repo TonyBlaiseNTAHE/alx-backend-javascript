2b2f311fc1207d5bf2c3cd37374f93d2e965596a
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-06-04 11:40:03 +0200

    implement try and catch

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    return numerator / denominator;
-  } catch (error) {
-    throw new Error('cannot divide by 0');
+  if (denominator === 0) {
+    throw  Error('cannot divide by 0');
+  } else {
+    return (numerator / denominator);
   }
 }
