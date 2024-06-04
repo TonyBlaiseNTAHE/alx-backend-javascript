f797fc4c7305fa373ad5aea56b396a22c0394850
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-06-04 15:50:35 +0200

    task10

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,17 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default async function asyncUploadUser() {
-  const upPhoto = await uploadPhoto();
-  const creUser = await createUser();
-
-  if (upPhoto === null || creUser === null) {
+  try {
+    const upPhoto = await uploadPhoto();
+    const creUser = await createUser();
+    return {
+      photo: upPhoto,
+      user: creUser,
+    };
+  } catch (error) {
     return {
       photo: null,
       user: null,
     };
   }
-  return {
-    photo: upPhoto,
-    user: creUser,
-  };
 }
