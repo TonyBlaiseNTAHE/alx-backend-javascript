f4afc0e3b0584991d90fb42a759b6017c733f89e
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-06-05 20:37:41 +0200

    implementing a class named HobertonCourse

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,11 +1,7 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (name === String) {
-        this._name = name;
-    }
-    if (length === int) {
-        this._length = length;
-    }
+    this._name = name;
+    this._length = length;
     this._students = students;
   }
 
@@ -22,14 +18,23 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
+    if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+    }
     this._name = name;
   }
 
   set length(length) {
+    if (typeof length !== 'number') {
+        throw new TypeError('Length must be a number');
+    }
     this._length = length;
   }
 
   set students(students) {
+    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+        throw new TypeError('Students must be an array of strings');
+      }
     this._students = students;
   }
 }
