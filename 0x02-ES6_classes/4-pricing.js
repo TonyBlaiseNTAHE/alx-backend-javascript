cc3f59d3c9621b9f7b5aa8881f67684989183b6f
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-10-04 10:07:20 +0200

    task4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== "number") {
+      throw new TypeError("amount must be a number");
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError("Currency must be a number");
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+  get amount() {
+    return this._amount;
+  }
+  set amount(amount) {
+    if (typeof amount !== "number") {
+      throw new TypeError("amount must be a number");
+    }
+    this._amount = amount;
+  }
+  get currency() {
+    return this._currency;
+  }
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError("Currency must be a number");
+    }
+    this._currency = currency;
+  }
+  displayFullPrice() {
+    const { amount, currency } = this;
+    return `${amount} ${currency.name} (${currency._code})`;
+  }
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== "number") {
+      throw new TypeError("Amount must be a number");
+    }
+    if (typeof conversionRate !== "number") {
+      throw new TypeError("ConversionRate must be a number");
+    }
+    return amount * conversionRate;
+  }
+}
