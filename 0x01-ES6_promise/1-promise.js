c770e23e5b84515019cf81d48c07880015f2e8c2
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-10-09 11:53:02 +0200

    a function that a new promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,11 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: "Success" });
+      const res = { status: 200, body: "Success" };
+      resolve(res);
     } else {
-      reject("The fake API is not working currently");
+      const error_message = new Error("The fake API is not working currently");
+      reject(error_message);
     }
   });
 }
