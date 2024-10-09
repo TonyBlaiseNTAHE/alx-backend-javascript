bb16f7aa062eb6ae6f6be24629e5627417ca9098
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-10-09 11:47:23 +0200

    a function that returns a new promise according the argumnet passed to it

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,13 @@
+export default function uploadPhoto() {
+  return {
+    status: 200,
+    body: "photo-profile-1",
+  };
+}
+
+export default function createUser() {
+    return {
+        firstname: 'Guillaume',
+        lastName: 'Salva',
+    }
+}
