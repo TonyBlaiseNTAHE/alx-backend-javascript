1b5d72f71a86dba6e37c1074d198dd11ff12ef86
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-10-09 12:14:25 +0200

    a function that return a promise where the argument passed is a boolen

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,11 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      const res = { status: 200, body: "Success" };
-      resolve(res);
+      const data = { status: 200, body: "Success" };
+      resolve(data);
     } else {
-      const error_message = new Error("The fake API is not working currently");
-      reject(error_message);
+      const error = new Error("The fake API is not working currently");
+      reject(error);
     }
   });
 }
