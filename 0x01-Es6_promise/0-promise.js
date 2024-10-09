dca10fe4d517fcb6ccf40dd0a7ab73d400a1e897
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-10-09 11:08:42 +0200

    removing promise dir

--- 0x01-Es6_promise/0-promise.js
+++
@@ -1,8 +0,0 @@
-export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      //console.log("succeded");
-      resolve("succeded");
-    }, 200);
-  });
-}
