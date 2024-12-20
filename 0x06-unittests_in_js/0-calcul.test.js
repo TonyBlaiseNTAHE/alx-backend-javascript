a1c2b9302a6a5de0be60302f42e460320a4a9c4d
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-12-20 05:45:03 +0200

    updates

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,22 +1,12 @@
-const calculateNumber = require("./0-calcul.js");
 const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-    it('rounding of a', () => {
-        assert.equal(calculateNumber(15.78, 2), 18);
-        assert.equal(calculateNumber(1.2, 0), 1);
-        assert.equal(calculateNumber(3.5, 2), 6);
-    });
+  it('should return -4 when inputs are -1 and -2.7', () => {
+    assert.strictEqual(calculateNumber(-1, -2.7), -4);
+  });
 
-    it('rounding of b', () => {
-        assert.equal(calculateNumber(2, 15.78), 18);
-        assert.equal(calculateNumber(0, 1.2), 1);
-        assert.equal(calculateNumber(2, 3.5), 6);
-    });
-
-    it('suming of a and b', () => {
-        assert.equal(calculateNumber(15.78, 2.5), 19);
-        assert.equal(calculateNumber(1.2, 0.2), 1);
-        assert.equal(calculateNumber(3.5, 2.7), 7);
-    });
-})
+  it('should return -4 when inputs are -1.7 and -2', () => {
+    assert.strictEqual(calculateNumber(-1.7, -2), -4);
+  });
+});
--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,19 +1,17 @@
 function calculateNumber(type, a, b) {
-    const rounded_a = Math.round(a);
-    const rounded_b = Math.round(b);
-    
-    if (type === 'SUM') {
-      return rounded_a + rounded_b;
-    }
-    if (type === 'SUBTRACT') {
-      return rounded_a - rounded_b;
-    }
-    if (type === 'DIVIDE') {
-      if (rounded_b === 0) {
-        return 'Error';
-      }
-      return rounded_a / rounded_b;
-    }
+  a = Math.round(a);
+  b = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return a + b;
+    case 'SUBTRACT':
+      return a - b;
+    case 'DIVIDE':
+      return b !== 0 ? a / b : 'Error';
+    default:
+      return 'Error';
   }
-  
-  module.exports = calculateNumber;
+}
+
+module.exports = calculateNumber;
